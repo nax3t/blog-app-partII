06d1bc9575a9d8f77bc833de3113dee29e359e52
Ian Schoonover <iantskon@gmail.com>
2016-10-11 11:39:28 -0500

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011132844) do
+ActiveRecord::Schema.define(version: 20161011163915) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
