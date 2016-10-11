6ea00b6a744d3b10fdba772db83b73ddf90aa5f2
Ian Schoonover <iantskon@gmail.com>
2016-10-11 10:15:06 -0500

    Add U in CRUD for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,16 @@ class PostsController < ApplicationController
 		redirect_to @post
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
+
 	private
 
 	def post_params
