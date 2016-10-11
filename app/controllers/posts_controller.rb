46e8793bd5ac8b0094ae6ed2b541375e1cb97f9b
Ian Schoonover <iantskon@gmail.com>
2016-10-11 10:27:04 -0500

    Add D in CRUD for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+	before_action :find_post, except: [:index, :new, :create]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		# find_post being called before action is run
 	end
 
 	def new
@@ -18,18 +20,28 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		# find_post being called before action is run
 	end
 
 	def update
-		@post = Post.find(params[:id])
+		# find_post being called before action is run
 		@post.update(post_params)
 		redirect_to @post
+	end
+
+	def destroy
+		# find_post being called before action is run
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private
 
 	def post_params
 		params.require(:post).permit(:title, :author, :image, :body)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
