743e1ce27e46f946a46bfdb76e8afa2e0c55dc0d
Ian Schoonover <iantskon@gmail.com>
2016-10-12 09:06:46 -0500

    Add user reference to post and remove author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012140419) do
+ActiveRecord::Schema.define(version: 20161012140631) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -23,11 +23,12 @@ ActiveRecord::Schema.define(version: 20161012140419) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "author"
     t.string   "image"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
